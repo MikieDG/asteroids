5f9d8b2b43d7ebad5660483f67a5a3b67b1c9892
MikieDG <michaelgauldin664@gmail.com>
2026-01-08 22:55:46 +0100

    End of tonight. Added shooting and need to fix rapidfire issue

--- player.py
+++ player.py
@@ -1,12 +1,13 @@
 import pygame
 from circleshape import CircleShape
-from constants import LINE_WIDTH, PLAYER_RADIUS, PLAYER_SPEED, PLAYER_TURN_SPEED
-
+from constants import LINE_WIDTH, PLAYER_RADIUS, PLAYER_SPEED, PLAYER_TURN_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN_SECONDS
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shooting_cooldown = 0
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
@@ -30,6 +31,10 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+
+        self.shooting_cooldown -= dt
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
@@ -39,3 +44,14 @@ class Player(CircleShape):
         rotated_vector = unit_vector.rotate(self.rotation)
         rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
         self.position += rotated_with_speed_vector
+
+    def shoot(self):
+        if self.shooting_cooldown > 0:
+            self.shooting_cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
+            return
+        else:
+            shot = Shot(self.position.x, self.position.y)
+            direction = pygame.Vector2(0, 1)
+            direction = direction.rotate(self.rotation)
+            direction *= PLAYER_SHOOT_SPEED
+            shot.velocity = direction
