647c0f64dcc5840c6381dc0d2579ccaa5f5b2d3c
MikieDG <michaelgauldin664@gmail.com>
2026-01-08 20:57:24 +0100

    Added fixed fps of 60fps and circleshape class

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    run = True
-    while run:
+    clock = pygame.time.Clock()
+    dt = 0
+    while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
