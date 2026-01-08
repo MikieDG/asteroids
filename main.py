54e5fbe299c337e0f3252d593e0361346d17a40b
MikieDG <michaelgauldin664@gmail.com>
2026-01-08 20:48:45 +0100

    Added gameloop and pressing x actually quits the game

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+from logger import log_state
 import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
-
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    run = True
+    while run:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
