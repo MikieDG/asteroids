3d9c35af589d6e03c01f6b05b509db830d06344f
MikieDG <michaelgauldin664@gmail.com>
2026-01-08 21:39:08 +0100

    Completed player movement. Changed to using groups for the update() and draw()

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 from logger import log_state
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS
+from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        for _ in drawable:
+            _.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
